80b274c27285ce5fcfc6aef53842c7091b4f8bf6
Luiz Rogerio Scudeletti <rogerio.scudeletti@gmail.com>
2022-10-20 20:06:16 -0300

    Adicionados endpoints de AreaPousio.

---
+++ TerritorEx.Api/Interfaces/IAreaPousio.cs
@@ -0,0 +1,10 @@
+﻿using TerritorEx.Api.Models.Area;
+
+namespace TerritorEx.Api.Interfaces;
+
+public interface IAreaPousio
+{
+    IReadOnlyList<AreaPousio> RecuperarTodos();
+
+    IReadOnlyList<AreaPousio> RecuperarPorTerritorioId(int territorioId);
+}
