1cce43d1db52fe293834cd87d1bfd7b57ca1f341
Luiz Rogerio Scudeletti <rogerio.scudeletti@gmail.com>
2023-03-01 20:48:40 -0300

    Foi adicionado os repositórios de Área de Borda Chapada e Área Consolidada

---
+++ TerritorEx.Api/Repositories/AreaConsolidadaRepository.cs
@@ -0,0 +1,39 @@
+﻿using Dapper;
+using TerritorEx.Api.Helpers;
+using TerritorEx.Api.Models;
+
+namespace TerritorEx.Api.Repositories;
+
+public static class AreaConsolidadaRepository
+{
+    public static IReadOnlyList<Area> RecuperarTodos()
+    {
+        using var sqlConnection = Utils.RecuperarConexao();
+
+        const string query = @"SELECT AreaId,
+                                      TerritorioId,
+                                      SicarId,
+                                      Descricao,
+                                      AreaHectare,
+                                      Shape
+                                 FROM AreaConsolidada;";
+
+        return (IReadOnlyList<Area>)sqlConnection.Query<Area>(query);
+    }
+
+    public static IReadOnlyList<Area> RecuperarPorTerritorioId(int territorioId)
+    {
+        using var sqlConnection = Utils.RecuperarConexao();
+
+        const string query = @"SELECT AreaId,
+                                      TerritorioId,
+                                      SicarId,
+                                      Descricao,
+                                      AreaHectare,
+                                      Shape
+                                 FROM AreaConsolidada
+                                WHERE TerritorioId = @territorioId;";
+
+        return (IReadOnlyList<Area>)sqlConnection.Query<Area>(query, new { territorioId });
+    }
+}
